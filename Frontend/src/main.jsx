6bc4f03227db50ab597649f908e8982ea8265d99
kadak <gwkadak@gmail.com>
2025-11-29 00:28:06 +0530

    new changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
-import App from './App.jsx'
+
 import Homepage from './Homepage.jsx'
 import CourseEntryForm from './TeacherLogin.jsx'
 import CreativeAttendancePage from './videoUploadPage.jsx'
